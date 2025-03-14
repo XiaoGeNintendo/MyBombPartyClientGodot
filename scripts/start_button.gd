3b1c72d2994780d6ae9f703e6707f0711dd10021
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2025-03-14 11:05:52 +0800

    fix locale

--- scripts/start_button.gd
+++ scripts/start_button.gd
@@ -2,7 +2,8 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var preferred_language = OS.get_locale_language()
+	TranslationServer.set_locale(preferred_language)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
